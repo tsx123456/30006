85dbb9f1bd4b3ededdd0b0e468926cc30e8a47b1
上帝的梦 <1597691532@qq.com>
2025-08-26 11:52:44 +0800

    1

--- index.d20ed.js
+++ index.d20ed.js
@@ -1,4 +1,4 @@
-System.register(["./application.adeb6.js"], function (_export, _context) {
+System.register(["./application.c00ef.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
